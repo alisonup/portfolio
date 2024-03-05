3b514a0f320031b1b9764aabff1f5ce60e247f64
alisonup <upworkalison@gmail.com>
2024-03-05 03:51:09 -0300

    delete unused elements

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,16 +1,11 @@
 import { Container } from "./styles";
-import emailIcon from "../../assets/email-icon.svg";
-import phoneIcon from "../../assets/phone-icon.svg"
 import { Form } from "../Form/Form";
 
-
 export function Contact() {
-
   return (
     <Container id="contact">
-      <header>
-      </header>
+      <header></header>
       <Form></Form>
     </Container>
-  )
+  );
 }
